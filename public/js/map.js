e901e00a69becf7c8cd2d2dd5bdecbdaed4403b4
cmswalker <c.michael.s.walker@gmail.com>
2015-05-25 00:26:31 -0700

    commit before sidebar row change

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,7 @@
 var markerPlace = "";
 
+
+
 $(function () {
 
   var $sidebar = $('#left');
@@ -7,13 +9,11 @@ $(function () {
   var $modalButton = $('#modalActivateButton');
   //Hide Onload
   $modalButton.hide();
-  var $addressBox = $('#left h3')
-  //Hide Onload
-  $addressBox.hide();
+  
 
-   $sidebar.click(function () {
+  $sidebar.click(function () {
     console.log('assigned');
-  })
+  });
 
   //google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -35,15 +35,44 @@ $(function () {
   var north = 37.871814;
   var east = -122.273756;
 
+
+
   var markers = [];
 
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
+  var styles = [
+  {
+    stylers: [
+      { hue: "#00ffe6" },
+      { saturation: -20 }
+    ]
+  },{
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      { lightness: 100 },
+      { visibility: "simplified" }
+    ]
+  },{
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
+
+
+map.setOptions({styles: styles});
+
    var defaultBounds = new google.maps.LatLngBounds(
        new google.maps.LatLng(south, west),
        new google.maps.LatLng(north, east));
-   map.fitBounds(defaultBounds);
+
+    map.fitBounds(defaultBounds);
 
    // var point = new google.maps.LatLng(41.3, -96);
    // var marker = new google.maps.Marker({
@@ -100,10 +129,9 @@ $(function () {
       //fill markers array with current FINISHED search
       markers.push(marker);
 
-      // if (markers.length > 1) {
-      //   markers.splice(1, markers.length-1);
-      // }
-
+      var $addressBox = $('#addressDiv')
+      //Hide Onload
+      $addressBox.hide();
       //fade in Address above button with marker address info
       $addressBox.html(marker.title).fadeIn(1000);
       //fade in modalButton to post about the address
@@ -153,9 +181,8 @@ $(function () {
 
               function logEach(v,i, arr) {
                 console.log(v);
-                //VISIBLE PLACE ID BELOW
+                //VISIBLE PLACE ID BELOW, can also push address
                 //postArray.push(v['place_id']);
-                //postArray.push( 'Address : ' + marker.title)
                 postArray.push( 'Move-in Year : ' + v['moveInYear']);
                 postArray.push( 'Monthly Rent : ' + '$' + v['monthlyRent']);
                 postArray.push( 'Bedrooms : ' + v['bedrooms']);
@@ -186,6 +213,7 @@ $(function () {
                 }
                 var linebreak = '<hr>';
                 postArray.push(linebreak);
+
               }//END logEach function
 
               matchedPlaces.forEach(logEach)
@@ -194,8 +222,7 @@ $(function () {
 
               render(postArray, 'sidebar-wrapper', 'sidebar-template');
 
-              //console.log(matchedPlaces[0]['place_id'], 'individual');
-        })
+        });//END AJAX Request
 
       bounds.extend(place.geometry.location);
 
@@ -210,88 +237,11 @@ $(function () {
         map.setZoom(mapOptions.maxZoom);
         map.setCenter(marker.getPosition());
 
-        // $.get('/posts', {place_id: marker.placeId} , function (json) {
-        //       console.log(json + 'dataaaaa');
-
-        //       var matchedPlaces = [];
-              
-        //       for (var i = 0; i < json.length; i ++) {
-        //         if (json[i].place_id === marker.placeId) {
-        //           console.log ('Success!');
-        //           matchedPlaces.push(json[i]);
-        //         }
-        //       }
-
-        //       //Template the markers Array into the sidebar
-        //       var render = function (items, parentId, templateId) {
-        //       console.log('rendering' + items)
-
-        //       var template = _.template($('#' + templateId).html());
-        //       $('#' + parentId).prepend(template({collection: items}));
-        //       }
-
-        //       console.log('checking markers' + markers);
-        //       console.log(markers);
-
-        //       //TURNED OFF TEMPLATING OF ADDRESS TO SIDEBAR
-        //       //render(markers, 'sidebar-wrapper', 'sidebar-template');
-              
-        //       var postArray = [];
-        //       console.log(postArray)
-
-        //       matchedPlaces.forEach(logEach)
-
-        //       function logEach(v,i, arr) {
-        //         console.log(v);
-        //         //postArray.push(v['place_id']);
-        //         postArray.push( 'Move-in Year : ' + v['moveInYear']);
-        //         postArray.push( 'Monthly Rent : ' + v['monthlyRent']);
-        //         postArray.push( 'Bedrooms : ' + v['bedrooms']);
-        //       }
-
-        //       console.log(postArray);
-
-        //       render(postArray, 'sidebar-wrapper', 'sidebar-template');
-
-        //       //console.log(matchedPlaces[0]['place_id'], 'individual');
-        // })
-
-
-      });
-
-
+      }); //End MarkerClick Event Listener
     }
-
-    // //Template the markers Array into the sidebar
-    // var render = function (items, parentId, templateId) {
-    // console.log('rendering' + items)
-    // var template = _.template($('#' + templateId).html());
-    // $('#' + parentId).prepend(template({collection: items}));
-    // }
-    // console.log('checking markers' + markers);
-    // console.log(markers);
-    // render(markers, 'sidebar-wrapper', 'sidebar-template');
-
-
-    //CURRENTLY PUTS ALL MARKER INFO INTO EACH MODAL, MAYBE NOT FOR EACH?  MAYBE LINK DIRECTLY TO DB?
-    //Template the MODAL 
-  
-
-    var renderModalPlaceID = function (items, parentId, templateId) {
-    console.log('rendering MODALS' + items)
-    var template = _.template($('#' + templateId).html());
-    $('#' + parentId).prepend(template({collection: items}));
-    }
-    console.log('checking markers for MODALS' + markers);
-    console.log(markers);
-
-    renderModalTitle(markers, 'placeID', 'modal-placeID-template');
-
-
     map.fitBounds(bounds);
   });
   //End Search EventListener
-
 
 
   // Bias the SearchBox results towards places that are within the bounds of the
